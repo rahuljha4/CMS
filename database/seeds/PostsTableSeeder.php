71581604d2bc6d15dddb50e9b157a5ac67a84878
Rahul Kumar <rahul2rkj@gmail.com>
2020-01-07 18:31:12 +0530

    Started working on Forntend of post

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,79 @@
+<?php
+
+use App\Post;
+use App\Category;
+use App\Tag;
+
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $category1 = Category::create([
+            'name' => 'News'
+        ]);
+
+        $category2 = Category::create([
+            'name' => 'Marketing'
+        ]);
+
+        $category3 = Category::create([
+            'name' => 'Partnership'
+        ]);
+
+        $post1 = Post::create([
+            'title' => 'We relocated our office to a new designed garage',
+            'description' => 'We relocated our office to a new designed garage',
+            'content' => 'We relocated our office to a new designed garage',
+            'category_id' => $category1->id,
+            'image' => 'posts/1.jpg'
+        ]);
+
+        $post2 = Post::create([
+            'title' => 'Top 5 brilliant content marketing strategies',
+            'description' => 'Top 5 brilliant content marketing strategies',
+            'content' => 'Top 5 brilliant content marketing strategies',
+            'category_id' => $category2->id,
+            'image' => 'posts/2.jpg'
+        ]);
+
+        $post3 = Post::create([
+            'title' => 'Best practices for minimalist design with example',
+            'description' => 'Best practices for minimalist design with example',
+            'content' => 'Best practices for minimalist design with example',
+            'category_id' => $category3->id,
+            'image' => 'posts/3.jpg'
+        ]);
+
+        $post4 = Post::create([
+            'title' => 'Congratulate and thank to Maryam for joining our team',
+            'description' => 'Congratulate and thank to Maryam for joining our team',
+            'content' => 'Congratulate and thank to Maryam for joining our team',
+            'category_id' => $category1->id,
+            'image' => 'posts/4.jpg'
+        ]);
+
+        $tag1 = Tag::create([
+            'name' => 'Record'
+        ]);
+
+        $tag2 = Tag::create([
+            'name' => 'Progress'
+        ]);
+
+        $tag3 = Tag::create([
+            'name' => 'Customers'
+        ]);
+
+        $post1->tags()->attach([$tag1->id, $tag2->id]);
+        $post2->tags()->attach([$tag2->id, $tag3->id]);
+        $post3->tags()->attach([$tag3->id, $tag1->id]);
+        $post4->tags()->attach([$tag1->id, $tag3->id]);
+    }
+}
